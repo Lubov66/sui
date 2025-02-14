038706dd8e3ba8da400a1716feded2049567be69
Mark Logan <103447440+mystenmark@users.noreply.github.com>
2025-02-14 13:29:14 -0800

    Rewrite and simplify checkpoint executor

--- crates/sui-core/src/authority.rs
+++ crates/sui-core/src/authority.rs
@@ -166,7 +166,7 @@ use crate::transaction_input_loader::TransactionInputLoader;
 use crate::transaction_manager::TransactionManager;
 
 #[cfg(msim)]
-pub use crate::checkpoints::checkpoint_executor::{
+pub use crate::checkpoints::checkpoint_executor::utils::{
     init_checkpoint_timeout_config, CheckpointTimeoutConfig,
 };
 
