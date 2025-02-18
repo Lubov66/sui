cf038104dc126ba80c1c95794527630366aaee55
Anastasios Kichidis <tasos@mystenlabs.com>
2025-02-18 10:53:04 +0000

    [chore] set gc_depth to 5.

--- crates/sui-protocol-config/src/lib.rs
+++ crates/sui-protocol-config/src/lib.rs
@@ -3205,7 +3205,7 @@ impl ProtocolConfig {
                     if chain != Chain::Mainnet && chain != Chain::Testnet {
                         // Assuming a round rate of max 15/sec, then using a gc depth of 60 allow blocks within a window of ~4 seconds
                         // to be included before be considered garbage collected.
-                        cfg.consensus_gc_depth = Some(60);
+                        cfg.consensus_gc_depth = Some(5);
                     }
 
                     if chain != Chain::Mainnet {
@@ -3245,12 +3245,6 @@ impl ProtocolConfig {
 
                     if chain != Chain::Mainnet && chain != Chain::Testnet {
                         cfg.feature_flags.consensus_linearize_subdag_v2 = true;
-                    }
-
-                    if chain != Chain::Mainnet {
-                        // Assuming a round rate of max 15/sec, then using a gc depth of 60 allow blocks within a window of ~4 seconds
-                        // to be included before be considered garbage collected.
-                        cfg.consensus_gc_depth = Some(60);
                     }
                 }
                 // Use this template when making changes:
